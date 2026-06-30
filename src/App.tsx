e53c287176fe40c9c0e5deada70c7505f35fb29d
lewislim <limyilewis@hotmail.com>
2026-06-30 17:43:27 +0800

    feat: scaffold canvas and test that spinning obj and lights works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 import "./App.css";
 
-function App() {
+function SpinningCube() {
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <>
-      <h1 className="text-3xl font-bold text-blue-500">Gacha Reveal</h1>
-    </>
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <h1 className="text-lg font-bold text-blue-500">Gacha Reveal</h1>
+      <div id="canvas-container" className="h-[500px] bg-pink-100">
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <SpinningCube />
+        </Canvas>
+      </div>
+    </>
+  );
+}
